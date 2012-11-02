4c33fdac721746e7177cce991da9c005fc6a677f
Steven Yang <kenshin520@gmail.com>
2012-11-02 11:55:58 +0800

    add test

---
+++ src/test/java/com/lunary/dbutils/database/SampleTest.java
@@ -0,0 +1,14 @@
+
+package com.lunary.dbutils.database;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+  @Test
+  public void test() {
+    Assert.assertTrue(false);
+  }
+}
